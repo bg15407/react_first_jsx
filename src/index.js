5d354953cbb58106016f6a553e101d018c99d3cd
Gayan Kalinga Bulathsinhala <bg15407@gmail.com>
2020-12-20 20:18:32 +0530

    added images

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import ReactDOM from "react-dom";
 const fName = "Gayan";
 const lName = "Kalinga";
 const myLuckyNumber = 7;
+const img = "https://picsum.photos/200";
 
 ReactDOM.render(
   <div>
@@ -20,6 +21,24 @@ ReactDOM.render(
       <li>Dulashi</li>
       <li>Miheli</li>
     </ul>
+    <div>
+      <img
+        alt="this is a d"
+        className="food-image"
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTlWKHTu_nGZiho0pz1CFAsqZ5tjWnbM5l2Mw&usqp=CAU"
+      ></img>
+      <img
+        alt="this is a c"
+        className="food-image"
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcadlHaCuNa63TpyxD23cIDRl3GEJguCUlKQ&usqp=CAU"
+      ></img>
+      <img
+        alt="this is a b"
+        className="food-image"
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRo27Gf3z4ArRXiZD3ykag7aNSpfFjP55emog&usqp=CAU"
+      ></img>
+      <img alt="this is a a" src="https://picsum.photos/200" />
+    </div>
     <p> My Lucky Number is {myLuckyNumber}</p>
   </div>,
   document.getElementById("root")
